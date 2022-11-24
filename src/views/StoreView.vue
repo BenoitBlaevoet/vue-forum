d083b851fc066c77bd52ef5475c0e37efef30f6b
Blaevoet Benoit <blaevoet.benoit@outlook.com>
2022-11-24 22:51:44 +0100

    initial commit

---
+++ src/views/StoreView.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <button @click="counter.increment">Count</button>
+    <p>Count is {{ counter.count }}</p>
+    <p>Double count is {{doubleCount}}</p>
+  </main>
+</template>
+
+<script setup>
+  import { useCounterStore } from '@/stores/counter'
+  import { storeToRefs } from 'pinia';
+  const counter = useCounterStore()
+  const { doubleCount } = storeToRefs(counter)
+</script>
