e39a84ad68799f62eba70860b1a97ac199d8f593
blaevoet benoit <blaevoet.benoit@outlook.com>
2022-11-28 10:42:34 +0100

    component test

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,45 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import vHeader from './components/vHeader.vue'
+import vHeaderItem from './components/vHeaderItem.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You o it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/store">Store</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <vHeader>
+    <vHeaderItem bgcolor="red" cols="5"></vHeaderItem>
+  </vHeader>
 
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+:root{
+  --primary: #019be5;
+  --primary-light:#63ccff;
+  --primary-dark:#006db3;
+  --secondary:#84ffff;
+  --secondary-light:#baffff;
+  --secondary-dark:#4bcbcc;
+  --text-color: black;
+  --text-color-light: white;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.bg-p{
+  background-color: var(--primary);
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.bg-pl{
+  background-color: var(--primary-light);
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.bg-pd{
+  background-color: var(--primary-dark);
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.bg-s{
+  background-color: var(--secondary);
 }
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.bg-sl{
+  background-color: var(--secondary-light);
 }
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.bg-sd{
+  background-color: var(--secondary-dark);
 }
 </style>
